fe0166d442d61695621188a412f8fe4cfe961149
Felix Hillingshaeuser <felix@mxcore.de>
2020-12-01 18:00:13 +0100

    add day02 to main.go

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
-import "github.com/unixlab/AoC2020/day01"
+import (
+	"github.com/unixlab/AoC2020/day02"
+)
 
 func main() {
 	// dayXX.Run()
-	day01.Run()
+	// day01.Run()
+	day02.Run()
 }
