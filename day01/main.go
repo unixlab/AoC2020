0ab056036d2c2f3a416923e27b00706798bdb754
Felix Hillingshaeuser <felix@mxcore.de>
2020-12-01 06:51:08 +0100

    add day01 part 1

--- day01/main.go
+++ day01/main.go
@@ -3,15 +3,30 @@ package day01
 import (
 	"bufio"
 	"fmt"
+	"gonum.org/v1/gonum/stat/combin"
 	"os"
+	"strconv"
 )
 
 func RunP1() {
 	file, _ := os.Open("day01/input.txt")
 	scanner := bufio.NewScanner(file)
 
+	var numbers []int
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+
+		number, _ := strconv.Atoi(line)
+		numbers = append(numbers, number)
+	}
+
+	// Part 1
+	combinationsIndex := combin.Combinations(len(numbers), 2)
+	for _, combinationIndex := range combinationsIndex {
+		if numbers[combinationIndex[0]] + numbers[combinationIndex[1]] == 2020 {
+			fmt.Printf("part 1 => %d\n", numbers[combinationIndex[0]]*numbers[combinationIndex[1]])
+			break
+		}
 	}
 }
