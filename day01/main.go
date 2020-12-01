24521444f46ebe893a5cbde47b8b736c7729d063
Felix Hillingshaeuser <felix@mxcore.de>
2020-12-01 06:51:29 +0100

    add day01 part 2

--- day01/main.go
+++ day01/main.go
@@ -29,4 +29,13 @@ func RunP1() {
 			break
 		}
 	}
+
+	// Part 2
+	combinationsIndex = combin.Combinations(len(numbers), 3)
+	for _, combinationIndex := range combinationsIndex {
+		if numbers[combinationIndex[0]] + numbers[combinationIndex[1]] + numbers[combinationIndex[2]] == 2020 {
+			fmt.Printf("part 2 => %d\n", numbers[combinationIndex[0]]*numbers[combinationIndex[1]]*numbers[combinationIndex[2]])
+			break
+		}
+	}
 }
