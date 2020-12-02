39a5348c3337621bd507a2cbe93b42870870194a
Felix Hillingshaeuser <felix@mxcore.de>
2020-12-02 06:20:30 +0100

    add day02 part 1

--- day02/main.go
+++ day02/main.go
@@ -4,14 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func Run() {
 	file, _ := os.Open("day02/input.txt")
 	scanner := bufio.NewScanner(file)
 
+	validCounterPart1 := 0
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		sections := strings.Split(line, " ")
+		min, _ := strconv.Atoi(strings.Split(sections[0], "-")[0])
+		max, _ := strconv.Atoi(strings.Split(sections[0], "-")[1])
+		char := strings.Replace(sections[1], ":", "", -1)
+		password := sections[2]
+
+		charCount := strings.Count(password, char)
+
+		if charCount >= min && charCount <= max {
+			validCounterPart1++
+		}
+
 	}
+
+	fmt.Printf("part 1 => %d\n", validCounterPart1)
 }
