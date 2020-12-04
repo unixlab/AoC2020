c28f1e73a94177007c6ff203b697d46ab2178091
Felix Hillingshaeuser <felix@mxcore.de>
2020-12-04 06:44:31 +0100

    add day04 part 2

--- day04/main.go
+++ day04/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -13,6 +15,7 @@ func Run() {
 
 	passport := make(map[string]string, 8)
 	validPassportsPart1 := 0
+	validPassportsPart2 := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -28,6 +31,57 @@ func Run() {
 
 			if passportOk {
 				validPassportsPart1++
+
+				byr, _ := strconv.Atoi(passport["byr"])
+				if byr < 1920 || byr > 2002 {
+					passportOk = false
+				}
+
+				iyr, _ := strconv.Atoi(passport["iyr"])
+				if iyr < 2010 || iyr > 2020 {
+					passportOk = false
+				}
+
+				eyr, _ := strconv.Atoi(passport["eyr"])
+				if eyr < 2020 || eyr > 2030 {
+					passportOk = false
+				}
+
+				hgt := passport["hgt"]
+				if strings.Index(hgt, "cm") >= 0 {
+					hgt = strings.Replace(hgt, "cm", "", 1)
+					hgtInt, _ := strconv.Atoi(hgt)
+					if hgtInt > 193 || hgtInt < 150 {
+						passportOk = false
+					}
+				} else {
+					hgt = strings.Replace(hgt, "in", "", 1)
+					hgtInt, _ := strconv.Atoi(hgt)
+					if hgtInt < 59 || hgtInt > 76 {
+						passportOk = false
+					}
+				}
+
+				hcl := passport["hcl"]
+				colorRegex, _ := regexp.Compile("^#[0-9a-f]{6}$")
+				if !colorRegex.MatchString(hcl) {
+					passportOk = false
+				}
+
+				ecl := passport["ecl"]
+				if ecl != "amb" && ecl != "blu" && ecl != "brn" && ecl != "gry" && ecl != "grn" && ecl != "hzl" && ecl != "oth" {
+					passportOk = false
+				}
+
+				pid := passport["pid"]
+				pidRegex, _ := regexp.Compile("^[0-9]{9}$")
+				if !pidRegex.MatchString(pid) {
+					passportOk = false
+				}
+
+				if passportOk {
+					validPassportsPart2++
+				}
 			}
 			passport = make(map[string]string, 8)
 			continue
@@ -39,4 +93,5 @@ func Run() {
 	}
 
 	fmt.Printf("part 1 => %d\n", validPassportsPart1)
+	fmt.Printf("part 2 => %d\n", validPassportsPart2)
 }
