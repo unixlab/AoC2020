cace5af0c880002b34827e127416578dc74b0a62
Felix Hillingshaeuser <felix@mxcore.de>
2020-12-06 07:55:01 +0100

    add day06 part 2

--- day06/main.go
+++ day06/main.go
@@ -11,23 +11,41 @@ func Run() {
 	file, _ := os.Open("day06/input.txt")
 	scanner := bufio.NewScanner(file)
 
-	var sum int
+	var sumPart1 int
+	var sumPart2 int
+	var people int
 	questions := make(map[string]int, 26)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		if len(line) == 0 {
-			sum += len(questions)
+			sumPart1 += len(questions)
+
+			for _, v := range questions {
+				if v == people {
+					sumPart2++
+				}
+			}
+
 			questions = make(map[string]int, 26)
+			people = 0
+			continue
 		}
 
 		for _, c := range strings.Split(line, "") {
 			questions[c]++
 		}
+		people++
 	}
 
-	sum += len(questions)
+	sumPart1 += len(questions)
+	for _, v := range questions {
+		if v == people {
+			sumPart2++
+		}
+	}
 
-	fmt.Printf("part 1 => %d\n", sum)
+	fmt.Printf("part 1 => %d\n", sumPart1)
+	fmt.Printf("part 2 => %d\n", sumPart2)
 }
